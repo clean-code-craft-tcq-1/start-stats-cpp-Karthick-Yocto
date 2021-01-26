b5f5f61810d7acecf8add68165abe7d8462e4e5a
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-01-26 13:15:12 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,10 @@
 #include <vector>
-
+typedef struct stats
+{
+    float s_avg;
+    float s_min;
+    float s_max;
+}Stats;
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<>& data);
 }
