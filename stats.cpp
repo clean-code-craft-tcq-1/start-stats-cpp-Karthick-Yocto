6e3d181019672f8d5a7b7f0b0c4a92ef87ca9367
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-01-26 13:59:59 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -8,26 +8,26 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& data) {
    stat_ret.min=0;
    stat_ret.max=0;
    
-   if (data.empty()) //Check data is empty or not
+   if (!data.empty()) //Check data is empty or not
     {
-        stat_ret.average = std::nan("");
-        stat_ret.min = std::nan("");
-        stat_ret.max = std::nan("");
-    }
-    else 
-    {
-		/*Calculate average value*/
+        /*Calculate average value*/
         for (auto i : data)
         {
             sum += i;
         }
-		stat_ret.average = sum / data.size();
+	stat_ret.average = sum / data.size();
 		
-		/*Find maximum value*/
+	/*Find maximum value*/
         stat_ret.max = *max_element(data.begin(), data.end());
 		
-		/*Find minimum value*/
+	/*Find minimum value*/
         stat_ret.min = *min_element(data.begin(), data.end());
+    }
+    else 
+    {
+	stat_ret.average = std::nan("");
+        stat_ret.min = std::nan("");
+        stat_ret.max = std::nan("");
 	}
 	
 	return stat_ret;
